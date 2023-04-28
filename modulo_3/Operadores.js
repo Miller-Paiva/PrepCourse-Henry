c04f4523e3f2bd8f2ddc51cb42cb79584b9df3cf
Miller <miller19.mlpg@gmail.com>
2023-04-28 06:57:11 -0500

    Tercer commit: creacion de variables y operadores

---
+++ modulo_3/Operadores.js
@@ -0,0 +1,25 @@
+// Los operadores en JavaScript son el +,-,* y /
+
+//Suma: operacion matematica convencional 
+ console.log(3+5) 
+
+ //Resta:operacion matematica convencional
+ console.log(10-3)
+
+ //Multiplicacion: operacion matematica convencional
+ console.log(10*5)
+
+ //Division: operacion matematica convencional
+console.log(10/2)
+
+//Resto 
+console.log(10%2)
+
+//Precedencia de operadores 
+console.log(3+5*2-8)
+
+// Adicion y String's: concatenado de string's y valores numericos 
+console.log('Miller '+'Paiva')
+var nombre = 'Miller '
+var apellido= 'Gomez'
+console.log(nombre + apellido)
